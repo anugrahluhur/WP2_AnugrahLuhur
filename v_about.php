95f13a12b9fc1915585a1a042578d4a9d14d2975
anugrahluhur <90765276+anugrahluhur@users.noreply.github.com>
2021-10-22 15:13:54 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,27 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Anisa </li>
+        <li>Nama Belakang : Febriyani</li>
+        <li>Nama Lengkap : Disposable Face Mask 50pcs</li>
+    </ul>
+    <br>
+
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Jalan Haji Sidin 1, Duren Sawit</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li>Magelang</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Catur</li>
+    </ul>
+</section>
